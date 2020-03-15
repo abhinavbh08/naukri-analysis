f5609e5309fc2201c48723455781361c1d0de204
abhinavbh08 <abhinavbhatt.999@gmail.com>
2020-03-15 22:23:45 +0530

    Added page for single page testing using selenium.

---
+++ selenium_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Chrome
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+
+web_driver_address = r"C:\\Users\\hp\\Downloads\\chromeDriver\\chromedriver.exe"
+
+
+options = webdriver.ChromeOptions()
+driver = Chrome(web_driver_address)
+driver.implicitly_wait(30)
+site_add = "https://www.naukri.com/job-listings-Solution-Developer-data-Scientist-data-Analyst-Machine-Learning-Tatvic-Ahmedabad-2-to-4-years-060320901433?src=jobsearchDesk&sid=15842656797941&xp=1&px=1"
+driver.get(site_add)
+time.sleep(2)
+data = driver.page_source
+driver.close()
+data = BeautifulSoup(data)
+single_job = data.find(class_="top")
+salary = single_job.find(class_="salary").find("span").get_text()
+experience = single_job.find(class_="exp").find("span").get_text()
+location = single_job.find(class_="loc").find("a").get_text()
+abc = 5
